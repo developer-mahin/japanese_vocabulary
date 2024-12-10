adb8c592c1c1961ffe809f8c98ef320b6884037b
developer-mahin <mdmahin1310@gmail.com>
2024-12-10 15:35:08 +0600

    project init and schema design done

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import { Server } from "http";
+import app from "./app";
+
+const port = 5000;
+
+function main() {
+  const server: Server = app.listen(port, () => {
+    console.log(`app is listening on port ${port}`);
+  });
+}
+
+main();
